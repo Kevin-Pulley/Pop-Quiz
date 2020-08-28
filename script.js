75334dbc40e7f36d27fcdc439b29fd0f843b8f34
Kevin Pulley <skippy1025@gmail.com>
2020-08-28 17:25:14 -0500

    Added to HTML, Added questions, tried to push highscores and names to highscore div

--- script.js
+++ script.js
@@ -1,56 +1,67 @@
 //array of the quiz questions, available choices, and correct answers
 
 let question = [{
-    title: "This is question 1",
-    choices: ["answer1", "answer2", "answer3", "answer4"],
-    answer: "answer1"
+    title: "What was the miniature horse's name?",
+    choices: ["Lil' Wayne", "Lil' Dicky", "Lil' Ginuwine", "Lil' Sebastian"],
+    answer: "Lil' Sebastian"
 },
 {
-    title: "This is question 2",
-    choices: ["answer1", "answer2", "answer3", "answer4"],
-    answer: "answer2"
+    title: "Who broke the coffee pot, and pretended not to know?",
+    choices: ["Leslie", "Ron", "Donna", "Tom"],
+    answer: "Ron"
 },
 {
-    title: "This is question 3",
-    choices: ["answer1", "answer2", "answer3", "answer4"],
-    answer: "answer3"
+    title: "How did Andy break his leg in season 1?",
+    choices: ["Playing Football", "Skydiving", "Rave at a club", "Fell into a pit"],
+    answer: "Fell into a pit"
 },
 {
-    title: "This is question 4",
-    choices: ["answer1", "answer2", "answer3", "answer4"],
-    answer: "answer4"
+    title: "Who was Leslie's arch enemy in the city council?",
+    choices: ["Chris Traeger", "Jeremy Jamm", "John-Ralphio", "Tammy Two"],
+    answer: "Jeremy Jamm"
 },];
 
 //Global variables
 let score = 0;
-let currentQuestion = -1;
+let currentQuestion = 0;
 let timeLeft = 0;
 let time;
+let buttons = document.getElementById("buttons");
 let startBtn = document.getElementById("startBtn");
 let button1 = document.getElementById("button1");
 let button2 = document.getElementById("button2");
 let button3 = document.getElementById("button3");
 let button4 = document.getElementById("button4");
-let titleQuestion = document.getElementById("askQuestion");
+let titleQuestion = document.getElementById("titleQuestion");
 let hideStart = document.getElementById("quizBody");
-let buttonDiv = document.getElementById("questions");
+let buttonDiv = document.getElementById("buttonDiv");
 let highScores = document.getElementById("left");
+let scorePrompt = document.getElementById("scorePrompt");
+let scoreEnter = document.getElementById("scoreEnter");
+let scoreScreen = document.getElementById("scoreScreen");
+let highScoreLink = document.getElementById("highScoreLink");
+let scoreHolder = document.getElementById("scoreHolder");
+let highForm = document.getElementById("highForm")
+let scoreArray = [];
 
-//Hides question buttons
-//buttonDiv.setAttribute("style", "display: none")
+//Hides question buttons and high score screens
+buttonDiv.setAttribute("style", "visibility: hidden");
+scoreScreen.setAttribute("style", "visibility: hidden");
 
 function start() {
+
     hideStart.setAttribute("style", "display: none");
+    buttonDiv.setAttribute("style", "display:inline-block, text-align:center");
     timer();
     callQuestions();
 }
 
 
 
-
+//localStorage.clear()
 
 function timer() {
-    timeLeft = 90;
+    timeLeft = 60;
     document.getElementById("timeLeft").innerHTML = timeLeft;
 
 
@@ -66,7 +77,6 @@ function timer() {
 
 
 function callQuestions() {
-    currentQuestion++
     titleQuestion.innerHTML = question[currentQuestion].title;
     button1.innerHTML = question[currentQuestion].choices[0];
     button2.innerHTML = question[currentQuestion].choices[1];
@@ -76,35 +86,102 @@ function callQuestions() {
     button2.setAttribute("style", "display: inline-block");
     button3.setAttribute("style", "display: inline-block");
     button4.setAttribute("style", "display: inline-block");
-    console.log(callQuestions)
+    //console.log(callQuestions)
 }
 
 
-    function nextQuestion() {
-        console.log(this.textContent);
-        if (this.textContent === question[currentQuestion].answer) {
-            score += 40;
-        } else
-            score -= 0;
+function nextQuestion() {
+    //console.log(this.textContent);
+    currentQuestion++
+    if (this.textContent === question[currentQuestion].answer) {
+        score += 70;
+    } else {
+        score -= 0;
+        timeLeft -= 10;
+    }
+    callQuestions();
+    //console.log(question[currentQuestion]);
+    console.log(score)
+};
 
-       
-        callQuestions();
+init();
+
+function setScore() {
+    // Clear todoList element and update todoCountSpan
+    scoreHolder.innerHTML = "";
 
 
-        console.log(question[currentQuestion]);
-    };
+    // Render a new li for each name
+    for (var i = 0; i < scoreArray.length; i++) {
+        var addScore = scoreArray[i];
 
+        var li = document.createElement("li");
+        li.textContent = addScore;
+        li.setAttribute("data-index", i);
 
-    function setScore() { };
-    function getScore() { };
-    function endGame() {
-        clearInterval(timer);
+        scoreHolder.appendChild(li);
+    }
+}
+
+function init() {
+    // Get stored  from localStorage
+    // Parsing the JSON string to an object
+    var storedScores = JSON.parse(localStorage.getItem("scoreArray"));
+
+    // If scores were retrieved from localStorage, update the score array to it
+    if (storedScores !== null) {
+        scoreArray = storedScores;
     }
 
+    // Render scoreArray to the DOM
+    setScore();
+}
 
-    startBtn.addEventListener("click", start);
-    button1.addEventListener("click", nextQuestion);
-    button2.addEventListener("click", nextQuestion);
-    button3.addEventListener("click", nextQuestion);
-    button4.addEventListener("click", nextQuestion);
+function storeScores() {
+        // Stringify and set "scoreArray" key in localStorage to score array
+    localStorage.setItem("scoreArray", JSON.stringify(scoreArray));
+    console.log(localStorage)
+}
 
+// When form is submitted...
+    highForm.addEventListener("submit", function (event) {
+    event.preventDefault();
+
+    var scoreText = scoreEnter.value.trim();
+
+    // Return from function early if submitted scoreEnter is blank
+    if (scoreText === "") {
+        return;
+    }
+
+    // Add new scoreEnter to scoreArray array, clear the input
+    scoreArray.push(scoreEnter);
+    scoreEnter.value = "";
+
+    // Store updated scores in localStorage, re-render the list
+    storeScores();
+    setScore();
+});
+
+
+
+function checkScore() {
+    scoreScreen.setAttribute("style", "display: inline-block, text-align: center")
+    buttonDiv.setAttribute("style", "visibility: hidden")
+    hideStart.setAttribute("style", "visibility: hidden")
+};
+
+
+function endGame() {
+    clearInterval(timer);
+    alert("You've run out of time!!")
+}
+
+
+startBtn.addEventListener("click", start);
+button1.addEventListener("click", nextQuestion);
+button2.addEventListener("click", nextQuestion);
+button3.addEventListener("click", nextQuestion);
+button4.addEventListener("click", nextQuestion);
+highScoreLink.addEventListener("click", checkScore)
+//scoreEnter.addEventListener("enter", storeScores)
